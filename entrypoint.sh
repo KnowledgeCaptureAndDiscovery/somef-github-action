6a9f02a8245b2898ddf37e01c45f23bb5dc78133
Faruk D <fdiblen@users.noreply.github.com>
2021-04-01 12:51:43 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,13 @@ echo "Inputs:"
 echo "  codemeta-file: $1"
 echo "  repository: $2"
 
+somef --help
+
 if [ -f "$1" ]
 then
   echo "Codemeta file exists. Exiting."
 else
-  some configure --auto
+  somef configure --auto
   generation-success=$(somef describe --repo_url $2 --codemeta_out $1)
   echo "::set-output name=generation-success::$generation-success"
 fi
